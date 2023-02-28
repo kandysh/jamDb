fce91eb668d9d676a148a4f64dd8b3976923096c
kandysh <rajawat.k.029@gmail.com>
2023-03-01 00:15:45 +0530

    amazon rds

--- japi/src/main/java/com/jamdb/japi/JapiConfiguration.java
+++ japi/src/main/java/com/jamdb/japi/JapiConfiguration.java
@@ -10,21 +10,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class JapiConfiguration {
 
-    private final UserDetailsService userDetailsService;
-
-    public JapiConfiguration(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+//    private final UserDetailsService userDetailsService;
+//
+//    public JapiConfiguration(UserDetailsService userDetailsService) {
+//        this.userDetailsService = userDetailsService;
+//    }
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(encoder());
-        return authenticationProvider;
-    }
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider(){
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService(userDetailsService);
+//        authenticationProvider.setPasswordEncoder(encoder());
+//        return authenticationProvider;
+//    }
 }
