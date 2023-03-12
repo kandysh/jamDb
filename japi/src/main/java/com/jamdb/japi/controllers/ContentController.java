01b1f5f17c5c2f1ad4c23d6073af5b4e6e7d4d60
InspiredEnigma12477 <sakoreshivam3901@gmail.com>
2023-03-12 18:47:54 +0530

    completed with login logout and grid token

--- japi/src/main/java/com/jamdb/japi/controllers/ContentController.java
+++ japi/src/main/java/com/jamdb/japi/controllers/ContentController.java
@@ -1,27 +1,23 @@
 package com.jamdb.japi.controllers;
 
-import com.jamdb.japi.dto.ContentDetailsDto;
 import com.jamdb.japi.services.ContentService.ContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/content")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
-@CrossOrigin
 public class ContentController {
     private final ContentService contentService;
 
     @GetMapping("/list")
-    public ResponseEntity<List<ContentDetailsDto>> getContentList() {
-        return ResponseEntity.ok(contentService.listContent());
-    }
-    @GetMapping("/{contentId}")
-    public ResponseEntity<ContentDetailsDto> getContent(@PathVariable String contentId){
-        return ResponseEntity.ok(contentService.getContent(contentId));
+    public ResponseEntity<?> list() {
+        return ResponseEntity.ok(contentService.listAllContent());
     }
 
 }
